96552b32a3e19d54910c7a3cf5f0fed291077d08
arielbarreiros96 <arielbarreiros96@gmail.com>
2024-03-30 16:36:16 +0000

    Cambios en las vistas para incluir nuevos campos

--- open_academy/models/course.py
+++ open_academy/models/course.py
@@ -4,6 +4,6 @@ class Course(models.Model):
     _name = 'openacademy.course'
     _description = 'Course class'
 
-    title = fields.Char(_("Title"), required=True)
+    name = fields.Char(_("Name"), required=True)
     description = fields.Char()
     responsible = fields.Many2one('res.users', _("Responsible"))
