10f1d3653c5fe4fde74dfbbc1c55a7224b463c59
arielbarreiros96 <arielbarreiros96@gmail.com>
2024-03-30 12:15:03 +0000

    Nuevas vistas search y form al curso

--- open_academy/models/course.py
+++ open_academy/models/course.py
@@ -1,8 +1,8 @@
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 
 class Course(models.Model):
     _name = 'openacademy.course'
     _description = 'Course class'
 
-    title = fields.Char(required=True)
+    title = fields.Char(_("Title"),required=True)
     description = fields.Char()
