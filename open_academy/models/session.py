bae51b9c4c027422adc1bb332d8ae1de8f4c476d
arielbarreiros96 <arielbarreiros96@gmail.com>
2024-03-31 17:28:37 +0100

    Muevo campo many2many relacionado a res.partner Se crea la relación inversa desde res.partner para este campo nuevo

--- open_academy/models/session.py
+++ open_academy/models/session.py
@@ -6,7 +6,18 @@ class Session(models.Model):
 
     name = fields.Char(_("Name"),required=True)
     start_date = fields.Date(_("Start Date"))
-    duration = fields.Integer(_("Duration"),help=_("Duration in min"))
+    duration = fields.Integer(_("Duration"), help=_("Duration in min"))
     number_of_seats = fields.Integer(_("Number of seats"))
     instructor = fields.Many2one("res.partner", string=_("Instructor"))
     course_id = fields.Many2one('openacademy.course', _("Course"))
+    attendees_ids = fields.Many2many('res.partner', 'attendees_partner_rel', string=_("Attendees"))
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    # Not implemented on the form res.partner form view
+    session_ids = fields.Many2many(
+        'openacademy.session',
+        'attendees_partner_rel',
+        string='Sessions'
+    )
