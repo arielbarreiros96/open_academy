93e63305290aade624a165c0322f80b680596a37
arielbarreiros96 <arielbarreiros96@gmail.com>
2024-03-31 19:19:23 +0100

    Se agrega progress bar mostrando taken_seats

--- open_academy/models/session.py
+++ open_academy/models/session.py
@@ -8,10 +8,19 @@ class Session(models.Model):
     start_date = fields.Date(_("Start Date"))
     duration = fields.Integer(_("Duration"), help=_("Duration in min"))
     number_of_seats = fields.Integer(_("Number of seats"))
+    taken_seats = fields.Float(_("Taken Seats"), compute='_compute_taken_seats')
     # This shuould be a many2many, or the session_instructor_ids on res_partner should be a one2many related to this one
     instructor = fields.Many2one("res.partner", string=_("Instructor"))
     course_id = fields.Many2one('openacademy.course', _("Course"))
     attendees_ids = fields.Many2many('res.partner', 'attendees_partner_rel', string=_("Attendees"))
+
+    @api.depends('attendees_ids')
+    def _compute_taken_seats(self):
+        for record in self:
+            if record.number_of_seats != 0:
+                record.taken_seats = 100*(len(record.attendees_ids)/record.number_of_seats)
+            else:
+                record.taken_seats = 0
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
